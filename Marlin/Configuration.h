22c410a6266f66d6b3a7020c50c84c7965253628
Wesley Luk <wesleyluk@gmail.com>
2020-09-01 10:22:00 -0700

    Build area

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1092,7 +1092,7 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 190
+#define X_BED_SIZE 193
 #define Y_BED_SIZE 185
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
